6428b9b971f0c2e160bad7c3cabbb12b54dbee7e
mon <mon.dev005@gmail.com>
2026-04-18 22:20:54 +0800

    added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import { HeaderComponent } from "./components/layout/HeaderComponent";
 import { FooterComponent } from "./components/layout/FooterComponent";
 import { AboutScreen } from "./components/screen/AboutScreen";
 import { BirdsScreen } from "./components/screen/BirdsScreen";
+import { FaqScreen } from "./components/screen/FaqScreen";
+import { BlogScreen } from "./components/screen/BlogScreen";
+import { SupportScreen } from "./components/screen/SupportScreen";
 import { NotFound } from "./components/layout/NotFound";
+import { ScrollToTop } from "./components/layout/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <HeaderComponent />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/birds" element={<BirdsScreen />} />
         <Route path="/about" element={<AboutScreen />} />
+        <Route path="/faq" element={<FaqScreen />} />
+        <Route path="/blog" element={<BlogScreen />} />
+        <Route path="/support" element={<SupportScreen />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
