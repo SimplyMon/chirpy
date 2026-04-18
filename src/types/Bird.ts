6218320eaf1485010c8716a7469bc9e8edbc0bc5
mon <mon.dev005@gmail.com>
2026-04-18 22:31:29 +0800

    added conservation status code

--- src/types/Bird.ts
+++ src/types/Bird.ts
@@ -7,3 +7,26 @@ export interface Bird {
   family?: string;
   imageUrl?: string;
 }
+
+export interface BirdDetails {
+  summary: string | null;
+  habitat: string | null;
+  region: string | null;
+  diet: string | null;
+  funFact: string | null;
+  conservation: ConservationStatus;
+  mass: string | null;
+  length: string | null;
+  wikipediaUrl: string | null;
+}
+
+export type ConservationStatus =
+  | "LC"
+  | "NT"
+  | "VU"
+  | "EN"
+  | "CR"
+  | "EW"
+  | "EX"
+  | "DD"
+  | "UNKNOWN";
